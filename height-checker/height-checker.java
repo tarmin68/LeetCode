418e641418753ee63b3717457644c984febb7fc4
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-03-25 20:09:04 -0400

    Time: 1 ms (76.71%), Space: 36.8 MB (52.62%) - LeetHub

---
+++ height-checker/height-checker.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] temp = new int[heights.length];
+        for(int i = 0; i < heights.length; i++){
+            temp[i] = heights[i];
+        }
+        
+        Arrays.sort(temp);
+        int count = 0;
+        for(int i = 0; i < temp.length; i++){
+            if(temp[i] == heights[i])
+                count++;
+        }
+        
+        return temp.length - count;
+    }
+}
