2a363c614f637e73d1fd31d3d6987ecd1a6e3925
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-05-10 11:02:18 -0400

    Time: 29 ms (5.00%), Space: 40.9 MB (99.56%) - LeetHub

--- minimum-path-sum/minimum-path-sum.java
+++ minimum-path-sum/minimum-path-sum.java
@@ -2,24 +2,43 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
         
-        int[][] minPaths = new int[m][n];
-        minPaths[m - 1][n - 1] = grid[m - 1][n - 1];
+        PriorityQueue<Cell> q = new PriorityQueue();
+        q.add(new Cell(0, 0, grid[0][0]));
         
-       for(int i = m + n - 3; i >= 0; i--){
-           for(int r = 0; r < m; r++){
-               int c = i - r;
-               if(c >= n || c < 0)
-                   continue;
-               
-               minPaths[r][c] = Integer.MAX_VALUE;
-               if(r < m - 1)
-                   minPaths[r][c] = minPaths[r + 1][c] + grid[r][c];
-               if(c < n - 1)
-                   minPaths[r][c] = Math.min(minPaths[r][c], minPaths[r][c + 1] + grid[r][c]);
-            }
+        while(!q.isEmpty()){
+            Cell currCell = q.poll();
+            if(currCell.r == m - 1 && currCell.c == n - 1)
+                return currCell.pathSum;
+            
+            visited[currCell.r][currCell.c] = true;
+            if(currCell.r < m - 1 && !visited[currCell.r + 1][currCell.c])
+                q.add(new Cell(currCell.r + 1, currCell.c, 
+                               currCell.pathSum + grid[currCell.r + 1][currCell.c]));
+            if(currCell.c < n - 1  && !visited[currCell.r][currCell.c + 1])
+                q.add(new Cell(currCell.r, currCell.c + 1,
+                               currCell.pathSum + grid[currCell.r][currCell.c + 1]));
         }
         
-        return minPaths[0][0];
+        return -1;
+    }
+    
+    public class Cell implements Comparable<Cell>{
+        int r;
+        int c;
+        int pathSum;
+        
+        public Cell(int r, int c, int pathSum){
+            this.r = r;
+            this.c = c;
+            this.pathSum = pathSum;
+        }
+        
+        @Override
+        public int compareTo(Cell ce){
+            return Integer.compare(this.pathSum, ce.pathSum);
+        }
     }
 }
+
