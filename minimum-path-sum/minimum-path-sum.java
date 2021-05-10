1066f7ca5286b01156a1c8d05a4dbeb3f17c62b8
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-05-10 11:09:29 -0400

    Time: 3 ms (19.25%), Space: 41.5 MB (69.24%) - LeetHub

--- minimum-path-sum/minimum-path-sum.java
+++ minimum-path-sum/minimum-path-sum.java
@@ -2,43 +2,21 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
         
-        PriorityQueue<Cell> q = new PriorityQueue();
-        q.add(new Cell(0, 0, grid[0][0]));
+        int[][] minPaths = new int[m][n];
         
-        while(!q.isEmpty()){
-            Cell currCell = q.poll();
-            if(currCell.r == m - 1 && currCell.c == n - 1)
-                return currCell.pathSum;
-            
-            visited[currCell.r][currCell.c] = true;
-            if(currCell.r < m - 1 && !visited[currCell.r + 1][currCell.c])
-                q.add(new Cell(currCell.r + 1, currCell.c, 
-                               currCell.pathSum + grid[currCell.r + 1][currCell.c]));
-            if(currCell.c < n - 1  && !visited[currCell.r][currCell.c + 1])
-                q.add(new Cell(currCell.r, currCell.c + 1,
-                               currCell.pathSum + grid[currCell.r][currCell.c + 1]));
-        }
+       for(int i = m - 1; i >= 0; i--){
+           for(int j = n - 1; j >= 0; j--){
+               minPaths[i][j] = Integer.MAX_VALUE;
+               if(i < m - 1)
+                    minPaths[i][j] = minPaths[i + 1][j] + grid[i][j];
+               if(j < n - 1)
+                    minPaths[i][j] = Math.min(minPaths[i][j], minPaths[i][j + 1] + grid[i][j]);
+               if(i == m - 1 && j == n - 1)
+                   minPaths[i][j] = grid[i][j];
+           }
+       }
         
-        return -1;
-    }
-    
-    public class Cell implements Comparable<Cell>{
-        int r;
-        int c;
-        int pathSum;
-        
-        public Cell(int r, int c, int pathSum){
-            this.r = r;
-            this.c = c;
-            this.pathSum = pathSum;
-        }
-        
-        @Override
-        public int compareTo(Cell ce){
-            return Integer.compare(this.pathSum, ce.pathSum);
-        }
+        return minPaths[0][0];
     }
 }
-
