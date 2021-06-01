be4a59560ea75a711a3c8de4735a1107cf609398
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-06-01 11:52:33 -0400

    Time: 5 ms (14.22%), Space: 44.7 MB (5.21%) - LeetHub

--- max-area-of-island/max-area-of-island.java
+++ max-area-of-island/max-area-of-island.java
@@ -1,57 +1,26 @@
 class Solution {
-    boolean[][] isVisited;
-    int max = 0;
-    
+    boolean isVisited[][];
     public int maxAreaOfIsland(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        isVisited = new boolean[n][m];
+        if(grid.length == 0) return 0;
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1 && !isVisited[i][j]){
-                    getIslandArea(grid, i, j);
-                }
+        int maxArea = 0;
+        isVisited = new boolean[grid.length][grid[0].length];
+        
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                int currArea = getArea(i, j, grid);
+                maxArea = Math.max(maxArea, currArea);
             }
         }
         
-        return max;
+        return maxArea;
     }
     
-    public void getIslandArea(int[][] grid, int i, int j){
-        int count = 0;
-        Queue<Pair<Integer, Integer>> q = new LinkedList();
-        q.add(new Pair(i, j));
+    public int getArea(int i, int j, int[][] grid){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || isVisited[i][j])
+            return 0;
+        
         isVisited[i][j] = true;
-        count++;
-        
-        while(!q.isEmpty()){
-            Pair<Integer, Integer> currCell = q.poll();
-            int row = currCell.getKey();
-            int col = currCell.getValue();
-            
-            if(row > 0 && grid[row - 1][col] == 1 && !isVisited[row - 1][col]){
-                q.add(new Pair(row - 1, col));
-                isVisited[row - 1][col] = true;
-                count++;
-            }
-            if(row < grid.length - 1 && grid[row + 1][col] == 1 && !isVisited[row + 1][col]){
-                q.add(new Pair(row + 1, col));
-                isVisited[row + 1][col] = true;
-                count++;
-            }
-            if(col > 0 && grid[row][col - 1] == 1 && !isVisited[row][col - 1]){
-                q.add(new Pair(row, col - 1));
-                isVisited[row][col - 1] = true;
-                count++;
-            }
-            if(col < grid[0].length - 1 && grid[row][col + 1] == 1 && !isVisited[row][col + 1]){
-                q.add(new Pair(row, col + 1));
-                isVisited[row][col + 1] = true;
-                count++;
-            }
-        }
-        
-        max = Math.max(max, count);
+        return 1 + getArea(i - 1, j, grid) + getArea(i + 1, j, grid) + getArea(i, j - 1, grid) + getArea(i, j + 1, grid);
     }
 }
