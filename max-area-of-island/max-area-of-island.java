d77811b3d6a40ca58c94a7a157774e119230a099
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-09-21 13:05:48 -0400

    Time: 3 ms (60.95%), Space: 40 MB (38.54%) - LeetHub

--- max-area-of-island/max-area-of-island.java
+++ max-area-of-island/max-area-of-island.java
@@ -1,26 +1,38 @@
 class Solution {
-    boolean isVisited[][];
+    boolean[][] isVisited;
+    
     public int maxAreaOfIsland(int[][] grid) {
-        if(grid.length == 0) return 0;
-        
-        int maxArea = 0;
         isVisited = new boolean[grid.length][grid[0].length];
-        
+        int max = 0;
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
-                int currArea = getArea(i, j, grid);
-                maxArea = Math.max(maxArea, currArea);
+                if(grid[i][j] == 1 && !isVisited[i][j]){
+                    int currMax = getMaxArea(grid, i, j);
+                    max = Math.max(max, currMax);
+                }
             }
         }
         
-        return maxArea;
+        return max;
     }
     
-    public int getArea(int i, int j, int[][] grid){
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || isVisited[i][j])
-            return 0;
+    public int getMaxArea(int[][] grid, int row, int col){
+        isVisited[row][col] = true;
+        int count = 1;
         
-        isVisited[i][j] = true;
-        return 1 + getArea(i - 1, j, grid) + getArea(i + 1, j, grid) + getArea(i, j - 1, grid) + getArea(i, j + 1, grid);
+        if(row > 0 && grid[row - 1][col] == 1 && !isVisited[row - 1][col]){
+            count += getMaxArea(grid, row - 1, col);
+        }
+        if(row + 1 < grid.length && grid[row + 1][col] == 1 && !isVisited[row + 1][col]){
+            count += getMaxArea(grid, row + 1, col);
+        }
+        if(col > 0 && grid[row][col - 1] == 1 && !isVisited[row][col - 1]){
+            count += getMaxArea(grid, row, col - 1);
+        }
+        if(col + 1 < grid[0].length && grid[row][col + 1] == 1 && !isVisited[row][col + 1]){
+            count += getMaxArea(grid, row, col + 1);
+        }
+        
+        return count;
     }
 }
