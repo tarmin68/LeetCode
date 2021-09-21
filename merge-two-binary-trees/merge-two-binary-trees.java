fb74407b749e11fc874ac14cb5f93fdd4d61eed0
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-09-21 11:57:11 -0400

    Time: 0 ms (100.00%), Space: 39.5 MB (42.39%) - LeetHub

--- merge-two-binary-trees/merge-two-binary-trees.java
+++ merge-two-binary-trees/merge-two-binary-trees.java
@@ -22,10 +22,10 @@ class Solution {
         if(root2 == null)
             return root1;
         
-        TreeNode root = new TreeNode(root1.val + root2.val);
-        root.left = mergeTrees(root1.left, root2.left);
-        root.right = mergeTrees(root1.right, root2.right);
+        TreeNode newRoot = new TreeNode(root1.val + root2.val);
+        newRoot.left = mergeTrees(root1.left, root2.left);
+        newRoot.right = mergeTrees(root1.right, root2.right);
         
-        return root;
+        return newRoot;
     }
 }
