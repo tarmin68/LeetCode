ec54db57a2d4f624157b4a044e9880da5bc4acc0
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-10-20 12:14:47 -0400

    Time: 1 ms (100.00%), Space: 41.1 MB (54.73%) - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,46 +1,22 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-        
-        int l = 0; 
-        int r = n - 1;
-        int i = n - 1;
-        while(i >= 0){
-            if(nums[r] * nums[r] > nums[l] * nums[l]){
-                res[i] = nums[r] * nums[r];
-                i--;
-                r--;
-            }
-            else{
-                res[i] = nums[l] * nums[l];
-                i--;
-                l++;
-            }
-        }
-        
-        return res;
-    }
-    
-    public int[] sortAllPos(int[] nums){
         int[] res = new int[nums.length];
         
-        for(int i = 0; i < nums.length; i++){
-            res[i] = nums[i] * nums[i];
+        int i = 0;
+        int j = nums.length - 1;
+        int k = nums.length - 1;
+        
+        while(i <= j){
+            if(nums[i] * nums[i] > nums[j] * nums[j]){
+                res[k--] = nums[i] * nums[i];
+                i++;
+            }
+            else{
+                res[k--] = nums[j] * nums[j];
+                j--;
+            }
         }
         
         return res;
     }
-    
-    public int[] sortAllNeg(int[] nums){
-        int n = nums.length;
-        int[] res = new int[n];
-        
-        for(int i = 0; i < n; i++){
-            res[i] = nums[n - 1 - i] * nums[n - 1 - i];
-        }
-        
-        return res;
-    }
-    
 }
