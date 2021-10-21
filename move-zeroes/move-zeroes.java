b1bf9089037b96a5b44bbf18b066b95f0a23302b
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-10-21 11:41:48 -0400

    Time: 2 ms (32.00%), Space: 52.6 MB (7.27%) - LeetHub

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -3,13 +3,12 @@ class Solution {
         int j = 0;
         
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] != 0){
+            if(nums[i] != 0)
                 nums[j++] = nums[i];
-            }
         }
         
-        for(int k = j; k < nums.length; k++){
-            nums[k] = 0;
+        for(int i = j; i < nums.length; i++){
+            nums[i] = 0;
         }
     }
 }
