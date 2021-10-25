fb9b98628d3be5980e9dfb07559b564a92679f71
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-10-25 15:42:39 -0400

    Time: 13 ms (6.02%), Space: 51.5 MB (5.06%) - LeetHub

--- max-area-of-island/max-area-of-island.java
+++ max-area-of-island/max-area-of-island.java
@@ -1,36 +1,33 @@
 class Solution {
-    boolean[][] isVisited;
-    
     public int maxAreaOfIsland(int[][] grid) {
-        isVisited = new boolean[grid.length][grid[0].length];
         int max = 0;
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 1 && !isVisited[i][j]){
-                    int currMax = getMaxArea(grid, i, j);
-                    max = Math.max(max, currMax);
-                }
+                max = Math.max(max, getMax(grid, i, j));
             }
         }
         
         return max;
     }
     
-    public int getMaxArea(int[][] grid, int row, int col){
-        isVisited[row][col] = true;
-        int count = 1;
+    public int getMax(int[][] grid, int sr, int sc){
+        Queue<int[]> q = new LinkedList();
+        q.add(new int[]{sr, sc});
+        int count = 0;
         
-        if(row > 0 && grid[row - 1][col] == 1 && !isVisited[row - 1][col]){
-            count += getMaxArea(grid, row - 1, col);
-        }
-        if(row + 1 < grid.length && grid[row + 1][col] == 1 && !isVisited[row + 1][col]){
-            count += getMaxArea(grid, row + 1, col);
-        }
-        if(col > 0 && grid[row][col - 1] == 1 && !isVisited[row][col - 1]){
-            count += getMaxArea(grid, row, col - 1);
-        }
-        if(col + 1 < grid[0].length && grid[row][col + 1] == 1 && !isVisited[row][col + 1]){
-            count += getMaxArea(grid, row, col + 1);
+        while(!q.isEmpty()){
+            int[] currCell = q.remove();
+            int r = currCell[0];
+            int c = currCell[1];
+            
+            if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0)
+                continue;
+            count++;
+            grid[r][c] = 0;
+            q.add(new int[]{r + 1, c});
+            q.add(new int[]{r - 1, c});
+            q.add(new int[]{r, c + 1});
+            q.add(new int[]{r, c - 1});
         }
         
         return count;
