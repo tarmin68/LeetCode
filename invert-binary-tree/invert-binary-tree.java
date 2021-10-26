ce5acc54723d3119b2b7c79dfc94f178cd31002d
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-10-26 10:47:27 -0400

    Time: 0 ms (100.00%), Space: 36.5 MB (71.17%) - LeetHub

--- invert-binary-tree/invert-binary-tree.java
+++ invert-binary-tree/invert-binary-tree.java
@@ -16,10 +16,12 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root == null)
-            return null;
-        TreeNode left = root.left;
-        root.left = invertTree(root.right);
-        root.right = invertTree(left);
+            return root;
+        
+        TreeNode left = invertTree(root.right);
+        TreeNode right = invertTree(root.left);
+        root.left = left;
+        root.right = right;
         return root;
     }
 }
