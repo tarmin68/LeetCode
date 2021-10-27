7e29f9b7ca696507a35dd8357525ed875cac8e9e
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-10-27 12:05:21 -0400

    Time: 7 ms (88.93%), Space: 42.8 MB (61.31%) - LeetHub

---
+++ 01-matrix/01-matrix.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] res = new int[m][n];
+        
+        for(int i = 0; i < m; i++){
+            Arrays.fill(res[i], m + n);
+        }
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 0)
+                    res[i][j] = 0;
+                else{
+                    if(i > 0)
+                        res[i][j] = Math.min(res[i][j], res[i - 1][j] + 1);
+                    if(j > 0)
+                        res[i][j] = Math.min(res[i][j], res[i][j - 1] + 1);
+                }
+            }
+        }
+        
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i < m - 1)
+                    res[i][j] = Math.min(res[i][j], res[i + 1][j] + 1);
+                if(j < n - 1)
+                    res[i][j] = Math.min(res[i][j], res[i][j + 1] + 1);
+            }
+        }
+        
+        return res;
+    }
+}
