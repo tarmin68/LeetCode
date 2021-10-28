38912f225610716117b606d7fe3f5881bc9eec0e
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-10-28 11:28:39 -0400

    Time: 126 ms (33.59%), Space: 44.9 MB (48.74%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,47 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        HashMap<Integer, ArrayList<Integer>> usedPair = new HashMap<>();
+        HashSet<Pair<Integer, Integer>> allPairs = new HashSet();
         
-        int zeroFreq = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0) zeroFreq++;
-            if(usedPair.get(nums[i]) == null){
-                usedPair.put(nums[i], new ArrayList<Integer>());
-                usedPair.get(nums[i]).add(nums[i]);
-            }
-            else{
-                usedPair.get(nums[i]).remove(new Integer(nums[i]));
-            }
-        }
-        
-        List<List<Integer>> res = new ArrayList<>();
-        if(zeroFreq > 2){
-            res.add(new ArrayList<Integer>(Arrays.asList(0, 0, 0)));
-            usedPair.get(0).add(0);
-        }
-        else if(zeroFreq > 0){
-            usedPair.get(0).add(0);
-        }
+        Arrays.sort(nums);
         
         for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(!usedPair.get(nums[i]).contains(nums[j])){
-                    int third = 0 - nums[i] - nums[j];
-                    if(!usedPair.get(nums[i]).contains(third) && !usedPair.get(nums[j]).contains(third) && usedPair.get(third) != null){
-                        ArrayList<Integer> currList = new ArrayList<>();
-                        currList.add(nums[i]);
-                        currList.add(nums[j]);
-                        currList.add(third);
-                        res.add(currList);
-                        usedPair.get(nums[i]).add(nums[j]);
-                        usedPair.get(nums[i]).add(third);
-                        usedPair.get(nums[j]).add(nums[i]);
-                        usedPair.get(nums[j]).add(third);
-                        usedPair.get(third).add(nums[i]);
-                        usedPair.get(third).add(nums[j]);
-                    }
+            int l = i + 1;
+            int r = nums.length - 1;
+            
+            while(l < r){
+                if(nums[i] + nums[l] + nums[r] == 0){
+                    allPairs.add(new Pair(nums[i], nums[l]));
+                    l++;
+                    r--;
                 }
+                else if(nums[i] + nums[l] + nums[r] < 0)
+                    l++;
+                else
+                    r--;
             }
+        }
+        
+        List<List<Integer>> res = new ArrayList();
+        for(Pair<Integer, Integer> currPair : allPairs){
+            int first = currPair.getKey();
+            int second = currPair.getValue();
+            int third = 0 - first - second;
+            List<Integer> currTriplet = Arrays.asList(new Integer[]{first, second, third});
+            res.add(currTriplet);
         }
         
         return res;
