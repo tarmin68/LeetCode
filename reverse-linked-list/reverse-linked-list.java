c3d154d8ebefe23fbddb4d76378ac279044cbe69
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-10-28 12:51:19 -0400

    Time: 0 ms (100.00%), Space: 39.1 MB (46.12%) - LeetHub

--- reverse-linked-list/reverse-linked-list.java
+++ reverse-linked-list/reverse-linked-list.java
@@ -10,16 +10,21 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        if(head == null)
+            return head;
+        
         ListNode prev = null;
         ListNode curr = head;
+        ListNode next = curr.next;
         
-        while(curr != null){
-            ListNode next = curr.next;
+        while(next != null){
             curr.next = prev;
             prev = curr;
             curr = next;
+            next = next.next;
         }
         
-        return prev;
+        curr.next = prev;
+        return curr;
     }
 }
