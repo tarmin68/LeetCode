abbb0b1f2d98543b7200d556fc2f9a0fc5b5e2f9
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2021-11-12 11:24:12 -0500

    Time: 6 ms (34.41%), Space: 38 MB (99.98%) - LeetHub

--- triangle/triangle.java
+++ triangle/triangle.java
@@ -1,29 +1,21 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        List<Integer> prevMin = new ArrayList();
-        prevMin.add(triangle.get(0).get(0));
+        List<Integer> prevRow = new ArrayList<Integer>();
+        prevRow.add(triangle.get(0).get(0));
         
         for(int i = 1; i < triangle.size(); i++){
-            List<Integer> currMin = new ArrayList();
-            for(int j = 0; j < triangle.get(i).size(); j++){
-                int currNum = triangle.get(i).get(j);
-                int min =  Integer.MAX_VALUE;
-                if(j > 0){
-                    min = Math.min(min, currNum + prevMin.get(j - 1));
-                }
-                if(j < prevMin.size()){
-                    min = Math.min(min, currNum + prevMin.get(j));
-                }
-                currMin.add(min);
+            List<Integer> currRow = new ArrayList<Integer>();
+            for(int j = 0; j < i + 1; j++){
+                int min = Integer.MAX_VALUE;
+                if(j > 0)
+                    min = Math.min(min, prevRow.get(j - 1) + triangle.get(i).get(j));
+                if(j < i)
+                    min = Math.min(min, prevRow.get(j) + triangle.get(i).get(j));
+                currRow.add(min);
             }
-            prevMin = currMin;
+            prevRow = currRow;
         }
         
-        int ans = Integer.MAX_VALUE;
-        for(int i = 0; i < prevMin.size(); i++){
-            ans = Math.min(ans, prevMin.get(i));
-        }
-        
-        return ans;
+        return Collections.min(prevRow);
     }
 }
