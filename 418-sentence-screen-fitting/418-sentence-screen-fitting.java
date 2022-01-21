90f3491c7e7f112cac23bba6a28e038145a69204
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2022-01-21 13:53:34 -0500

    Time: 20 ms (43.70%), Space: 46.5 MB (5.21%) - LeetHub

--- 418-sentence-screen-fitting/418-sentence-screen-fitting.java
+++ 418-sentence-screen-fitting/418-sentence-screen-fitting.java
@@ -1,30 +1,41 @@
 class Solution {
+    Integer[] counts;
+    Integer[] idxs;
+    
     public int wordsTyping(String[] sentence, int rows, int cols) {
+        counts = new Integer[sentence.length];
+        idxs = new Integer[sentence.length];
         int count = 0;
-        int row = 0;
-        int col = 0;
         int idx = 0;
         
-        while(row < rows){
-            if(col + sentence[idx].length() <= cols){
-                col += sentence[idx].length() + 1;
-                if(col >= cols){
-                    row++;
-                    col = 0;
-                }
-                
-                idx++;
-                if(idx == sentence.length){
-                    count++;
-                    idx = 0;
-                }
-            }
-            else{
-                row++;
-                col = 0;
-            }
+        for(int i = 0; i < rows; i++){
+            Pair<Integer, Integer> currRes = fitScreen(sentence, idx, cols);
+            idx = currRes.getKey();
+            count += currRes.getValue();
         }
         
         return count;
+    }
+    
+    public Pair<Integer, Integer> fitScreen(String[] sentence, int idx, int cols){
+        if(counts[idx] != null)
+            return new Pair(idxs[idx], counts[idx]);
+        
+        int count = 0;
+        int col = 0;
+        int newIdx = idx;
+        
+        while(col + sentence[newIdx].length() <= cols){
+            col += sentence[newIdx].length() + 1;
+            newIdx++;
+            if(newIdx == sentence.length){
+                count++;
+                newIdx = 0;
+            }
+        }
+        idxs[idx] = newIdx;
+        counts[idx] = count;
+        
+        return new Pair(newIdx, count);
     }
 }
