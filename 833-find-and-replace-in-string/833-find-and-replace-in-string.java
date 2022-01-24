8b20b67c31091a98366b63512a73a4eaa211c5fd
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2022-01-24 15:47:34 -0500

    Time: 19 ms (5.37%), Space: 39.8 MB (24.31%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.java
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.java
@@ -8,16 +8,34 @@ class Solution {
         
         Arrays.sort(replaces, (a, b) -> a.index - b.index);
         
-        StringBuilder res = new StringBuilder(s);
+        StringBuilder res = new StringBuilder();
         int idxDif = 0;
-        for(int i = 0; i < replaces.length; i++){
-            if(s.startsWith(replaces[i].source, replaces[i].index)){
-                int start = replaces[i].index + idxDif;
-                int end = start + replaces[i].source.length();
-                res.replace(start, end, replaces[i].target);
-                idxDif += (replaces[i].target.length() - replaces[i].source.length());
+        
+        int idx = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(idx < replaces.length && replaces[idx].index == i){
+                if(s.startsWith(replaces[idx].source, i)){
+                    res.append(replaces[idx].target);
+                    i += replaces[idx].source.length() - 1;
+                    System.out.println(i);
+                }
+                else{
+                    res.append(s.charAt(i));
+                }
+                idx++;
+            }
+            else{
+                res.append(s.charAt(i));
             }
         }
+        // for(int i = 0; i < replaces.length; i++){
+        //     if(){
+        //         int start = replaces[i].index + idxDif;
+        //         int end = start + replaces[i].source.length();
+        //         res.replace(start, end, replaces[i].target);
+        //         idxDif += (replaces[i].target.length() - replaces[i].source.length());
+        //     }
+        // }
         
         return res.toString();
     }
